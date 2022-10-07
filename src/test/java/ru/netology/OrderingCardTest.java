4e7e3c377d9bc87271779a006c474a7ad4f7769a
andreygork <agorkaev99@gmail.com>
2022-10-07 17:54:46 +0300

    Update Test

--- src/test/java/ru/netology/OrderingCardTest.java
+++ src/test/java/ru/netology/OrderingCardTest.java
@@ -31,15 +31,17 @@ public class OrderingCardTest {
     }
 
     @Test
-    public void shouldGetSuccess() {
-        driver.get("http://localhost:7777");
+    public void shouldGetSuccess() throws InterruptedException {
+        driver.get("http://localhost:7777/");
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Андрей");
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79017054075");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.className("button__content")).click();
+
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        Assertions.assertEquals(actual, expected);
 
+        Assertions.assertEquals(actual, expected);
+        Thread.sleep(50000);
     }
 }
